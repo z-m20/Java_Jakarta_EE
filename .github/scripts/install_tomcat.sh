af4f3fb4f5db699bef502ee9204397ee7d6a7595
Nicolas <Nicolas Rousset>
2021-04-20 19:54:37 +0200

    WiP

--- .github/scripts/install_tomcat.sh
+++ .github/scripts/install_tomcat.sh
@@ -1,4 +1,7 @@
 cd /tmp
+mkdir -p /opt/tomcat
+echo "=== downloading ==="
 curl -O https://www-us.apache.org/dist/tomcat/tomcat-10/v10.0.5/bin/apache-tomcat-10.0.5.tar.gz
-ls -l .
+ls -l /tmp
+echo "=== unzipping ==="
 tar xzvf /tmp/apache-tomcat-10.0.5.tar.gz -C /opt/tomcat --strip-components=1
