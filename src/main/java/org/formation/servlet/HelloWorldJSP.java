863c4c62cee102cfd50720e4b679058711eb5bae
Nicolas <Nicolas Rousset>
2021-04-20 17:19:25 +0200

    WiP

--- src/main/java/org/formation/servlet/HelloWorldJSP.java
+++ src/main/java/org/formation/servlet/HelloWorldJSP.java
@@ -12,7 +12,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.PushBuilder;
 
+@WebServlet("/HelloWorldJSP")
 public class HelloWorldJSP extends HttpServlet {
-	
-	req.getRequestDispatcher("/jsp/target.jsp").forward(req, resp);
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException,ServletException {
+		req.getRequestDispatcher("/jsp/target.jsp").forward(req, resp);
+	}
 }
